01ab139aa5889d0bd4d3967298ef0ca10e42875e
wangpeng116 <wangpeng116@longfor.com>
2020-11-03 16:31:58 +0800

    项目初始化

---
+++ src/main/java/com/wp/main/leetcode/LeetCodeOne.java
@@ -0,0 +1,19 @@
+package com.wp.main.leetcode;
+
+/**
+ * @Classname LeetCodeOne
+ * @Description 两数之和
+ * @Date 2020/11/3 16:26
+ * @Created by wangpeng116
+ */
+public class LeetCodeOne {
+    public static void main(String[] args) {
+        Integer[] nums = {1, 3, 4, 5, 6, 8, 9};
+        Integer target = 15;
+        execute(nums, target);
+    }
+
+    public static void execute(Integer[] nums, Integer target) {
+
+    }
+}
