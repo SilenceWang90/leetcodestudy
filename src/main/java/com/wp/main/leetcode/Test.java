e82c6e3e30cb5296ed32e423997b6c8ee3e6cd7b
wangpeng116 <wangpeng116@longfor.com>
2020-11-11 14:11:08 +0800

    栈操作了解

--- src/main/java/com/wp/main/leetcode/Test.java
+++ src/main/java/com/wp/main/leetcode/Test.java
@@ -14,6 +14,7 @@ public class Test {
     public static void main(String[] args) {
         Deque<String> stack = Lists.newLinkedList();
         Deque<String> queue = Lists.newLinkedList();
+        /**1、堆栈使用方法**/
         //1、堆栈压入数据
         stack.push("111");
         //2、查看堆栈头部数据，不会清除该数据
@@ -31,5 +32,18 @@ public class Test {
         //5、查看栈头和栈尾
         System.out.println("stack.peekFirst()：" + stack.peekFirst());
         System.out.println("stack.peekLast()：" + stack.peekLast());
+        /**2、队列使用方法：队列尾用于插入数据，队列头用于读取数据**/
+        //add()、offer()队列尾插入数据
+        /*queue.add("1");
+        queue.offer("2");
+        //First是向队列头部增加数据
+        queue.addFirst("0");
+        //Last是向队列尾部增加数据
+        queue.addLast("3");
+        //push()是向队列头部增加数据
+        queue.push("4");
+        for (String obj : queue) {
+            System.out.println(obj);
+        }*/
     }
 }
