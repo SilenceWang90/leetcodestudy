c4af32e143d3f87f6c2bc07271526d3c2aedf7b1
wangpeng116 <wangpeng116@longfor.com>
2020-11-16 13:28:20 +0800

    合并有序链表

--- src/main/java/com/wp/main/leetcode/easy/LeetCode21.java
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode21.java
@@ -6,10 +6,10 @@ import com.wp.main.leetcode.common.ListNode;
  * @Classname LeetCode21
  * @Description 合并两个有序链表
  * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
- * <p>
  * 示例：
- * <p>
- * 输入：1->2->4->7, 1->3->4->6
+ * 输入：
+ * listNode1：1->2->4->7
+ * listNode2：1->3->4->6
  * 输出：1->1->2->3->4->4->6->7
  * @Date 2020/11/16 13:08
  * @Created by wangpeng116
