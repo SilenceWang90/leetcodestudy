13f803218626cc38e9b2a27d2fc9f6a8b71f03c0
wangpeng116 <wangpeng116@longfor.com>
2020-11-16 14:09:30 +0800

    合并有序链表

--- src/main/java/com/wp/main/leetcode/easy/LeetCode21.java
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode21.java
@@ -42,19 +42,36 @@ public class LeetCode21 {
     }
 
     /**
-     * 一：当某个链表为空的时候，返回另一个不为空的链表即可
-     * 二：递归：
-     * 2.1：
-     * 2.2：
+     * 思路：
+     * 一、定义最终结果链表：
+     * 1.1、新增最终队列ListNode Result
+     * 1.2、preNode记录最终链表的最末尾的值，便于指向比较后的最新值
+     * 二、迭代比较
+     * 2.1、比较两个链表表头的值，新链表的末尾指向小的值
+     * 2.2、被指向的值的队列，队列头部节点后移（比如l1和l2的值中，l1的值小。那么result指向l1的当前值，所以l1后移一位便于继续比较）
      *
      * @param listNode1
      * @param listNode2
      * @return
      */
     private static ListNode standardExecute(ListNode listNode1, ListNode listNode2) {
-        //最终队列的头
+        //最终队列
         ListNode result = new ListNode(-1);
-
+        //最终队列末尾值
+        ListNode preNode = result;
+        while (listNode1 != null && listNode2 != null) {
+            if (listNode1.getValue() < listNode2.getValue()) {
+                //如果第一个链表的值小于第二个链表的值，则最终链表指向第一个链表
+                preNode.setNext(listNode1);
+                preNode = listNode1;
+                listNode1 = listNode1.getNext();
+            } else {
+                //如果第一个链表的值大于等于第二个链表的值，则最终链表指向第二个链表
+                preNode.setNext(listNode2);
+                preNode = listNode2;
+                listNode2 = listNode2.getNext();
+            }
+        }
 
 
         return result;
