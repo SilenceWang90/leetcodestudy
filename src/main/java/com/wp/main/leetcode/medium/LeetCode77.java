f7cf1ff3fc9f5144b0164dfe9b18a6c4e3ca928a
wangpeng116 <wangpeng116@longfor.com>
2020-11-24 16:27:56 +0800

    递归：实现选择。 剪枝优化：对不可能的情况无须遍历，剪枝即可。

--- src/main/java/com/wp/main/leetcode/medium/LeetCode77.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode77.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Lists;
 import com.wp.main.leetcode.common.BiddingSupplierInfo;
 
 import java.math.BigDecimal;
+import java.sql.Array;
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
 
@@ -32,20 +34,29 @@ public class LeetCode77 {
         BiddingSupplierInfo sup1 = new BiddingSupplierInfo().setSupplierId("1").setSupplierName("1供应商").setPrice(BigDecimal.valueOf(10)).setSectionId("标段一");
         BiddingSupplierInfo sup2 = new BiddingSupplierInfo().setSupplierId("2").setSupplierName("2供应商").setPrice(BigDecimal.valueOf(12)).setSectionId("标段一");
         BiddingSupplierInfo sup3 = new BiddingSupplierInfo().setSupplierId("3").setSupplierName("3供应商").setPrice(BigDecimal.valueOf(29)).setSectionId("标段一");
-        /*BiddingSupplierInfo sup4 = new BiddingSupplierInfo().setSupplierId("4").setSupplierName("4供应商").setPrice(BigDecimal.valueOf(37)).setSectionId("标段一");
+        BiddingSupplierInfo sup4 = new BiddingSupplierInfo().setSupplierId("4").setSupplierName("4供应商").setPrice(BigDecimal.valueOf(37)).setSectionId("标段一");
         BiddingSupplierInfo sup5 = new BiddingSupplierInfo().setSupplierId("5").setSupplierName("5供应商").setPrice(BigDecimal.valueOf(8)).setSectionId("标段一");
         BiddingSupplierInfo sup6 = new BiddingSupplierInfo().setSupplierId("6").setSupplierName("6供应商").setPrice(BigDecimal.valueOf(24)).setSectionId("标段一");
         BiddingSupplierInfo sup7 = new BiddingSupplierInfo().setSupplierId("1").setSupplierName("1供应商").setPrice(BigDecimal.valueOf(22)).setSectionId("标段二");
-        BiddingSupplierInfo sup8 = new BiddingSupplierInfo().setSupplierId("2").setSupplierName("2供应商").setPrice(BigDecimal.valueOf(19)).setSectionId("标段二");*/
+        BiddingSupplierInfo sup8 = new BiddingSupplierInfo().setSupplierId("2").setSupplierName("2供应商").setPrice(BigDecimal.valueOf(19)).setSectionId("标段二");
         //数据集合
-//        List<BiddingSupplierInfo> list = Lists.newArrayList(sup1, sup2, sup3, sup4, sup5, sup6, sup7, sup8);
-        List<BiddingSupplierInfo> list = Lists.newArrayList(sup1, sup2, sup3);
+        List<BiddingSupplierInfo> list = Lists.newArrayList(sup1, sup2, sup3, sup4, sup5, sup6, sup7, sup8);
+//        List<BiddingSupplierInfo> list = Lists.newArrayList(sup1, sup2, sup3);
         //已选集合(堆栈，因为我们为了让选择[1,2][1,3][1,n]组合，就必须在每次递归结束后清除栈顶数据，这样才能保证找出所有需要选择的数据)
+        //ArrayDeque优于LikedArrayList，因为ArrayDeque底层基于数组，LikedArrayList底层基于链表，对于当前业务ArrayDeque性能更高
         Deque<BiddingSupplierInfo> selectedStack = Lists.newLinkedList();
         //结果集合
         List<List<BiddingSupplierInfo>> result = Lists.newArrayList();
-        calculate(0, 2, list.size(), selectedStack, list, result);
-        System.out.println(result.toString());
+        //初始从第一个位置开始
+        long start = System.currentTimeMillis();
+        calculate(0, 3, list.size(), selectedStack, list, result);
+        long end = System.currentTimeMillis();
+        System.out.println(result.size());
+        System.out.println("执行时长：" + (end - start) + "毫秒");
+        /*for (List<BiddingSupplierInfo> obj : result) {
+            System.out.println(obj.toString());
+        }*/
+
     }
 
     /**
