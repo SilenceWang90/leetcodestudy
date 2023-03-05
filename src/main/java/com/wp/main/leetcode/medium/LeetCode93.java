9fbdee920a8c44f5d5bb1582735f8b646e967581
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-05 18:33:42 +0800

    LeetCode93：复原IP地址 个人思路：递归循环。递归逻辑已经形成，但是实现太麻烦......很多点的判断未完善 官方思路：和个人思路相同......

--- src/main/java/com/wp/main/leetcode/medium/LeetCode93.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode93.java
@@ -32,10 +32,84 @@ import java.util.List;
  */
 public class LeetCode93 {
     public static void main(String[] args) {
+//        System.out.println(individualExecute("1234567891234"));
+//        System.out.println(individualExecute("12"));
+//        System.out.println(individualExecute(""));
 //        System.out.println(individualExecute("0000"));
 //        System.out.println(individualExecute("25525511135"));
-        System.out.println(standardExecute("25525511135"));
-        System.out.println(standardExecute("0000"));
+        System.out.println(individualExecute("101023"));
+    }
+
+    /**
+     * 个人思路：递归
+     *
+     * @param s 给定的IP字符串
+     * @return 结果
+     */
+    @Deprecated
+    private static List<String> individualExecute(String s) {
+        List<String> result = Lists.newArrayList();
+        StringBuilder joint = new StringBuilder();
+        // 记录当前递归过程字符串的长度(用于递归结束后清理当前递归产生的字符串)，stack的大小是拆解的次数
+        Deque<Integer> stack = Lists.newLinkedList();
+        findValidIpStr(result, joint, s, stack);
+        return result;
+    }
+
+    @Deprecated
+    private static void findValidIpStr(List<String> result, StringBuilder joint, String str, Deque<Integer> stack) {
+        // 递归边界：字符串拆解3次即完成一个ip的查询
+        if (stack.size() == 3) {
+            // 符合IP规则，则加入到结果集中：
+            // （1）不为空
+            // （2）首位不是0，但如果有且只有0是可以的
+            // （3）数字小于等于255
+            if (str != null && !"".equals(str)
+                    && !(str.length() > 1 && str.charAt(0) == '0')
+                    && Integer.parseInt(str) <= 255) {
+                joint.append(str);
+                result.add(joint.toString());
+                // 记录完成后将拼接的字符串清除，用于下次遍历
+                joint.delete(joint.lastIndexOf(".") + 1, joint.length());
+            }
+            return;
+        }
+        // 经过类比发现：使用1个“.”进行字符串分隔，剩余的字符串长度不能超过9；使用2个“.”进行字符串分隔，剩余的字符串长度不能超过6；使用3个“.”进行字符串分隔，剩余的字符串长度不能超过3；
+        // 随着“.”的个数增加，剩余字符串数量减少。
+        // 因此，我们可以设定每增加1个"."和能形成有效IP的函数关系为：剩余字符串长度=(4-"当前使用'.'的个数")*3
+        if (str.length() <= (4 - stack.size()) * 3 && str.length() > 0) {
+            // 每层循环中，该层字符个数不能超过3，超过3本层循环就可以结束了
+            for (int i = 0; i < Math.min(3, str.length()); i++) {
+                String currentStr = str.substring(0, i + 1);
+                // 当层循环中：
+                // （1）数值大于255，则已经不符合IP规则，停止循环
+                // （2）该数值的首位是0(0可以，但是01，012这种就不行)，则已经不符合IP规则，停止循环
+                if (Integer.parseInt(currentStr) > 255 || (currentStr.length() > 1 && currentStr.charAt(0) == '0')) {
+                    break;
+                }
+                joint.append(currentStr).append(".");
+                // 放入当前字符串的长度，记得带上"."，因为joint的时候把"."也带上了
+                stack.push(currentStr.length() + 1);
+                // 截取剩余字符串
+                String remainStr = str.substring(i + 1);
+                findValidIpStr(result, joint, remainStr, stack);
+                // 底层递归完成，当前内容清理
+                int currentLength = stack.pop();
+                joint.delete(joint.length() - currentLength, joint.length());
+            }
+        }
+        /*else {
+            // 当前IP规则不满足
+            if (stack.size() > 0) {
+                // 不符合IP规则，清理递归回溯过程中最近记录的内容(其实就是上一次调用的内容，就因为上一次调用的方案不合理才会导致本次失败，索引要把上一次的内容清理掉)
+                int length = stack.pop();
+                joint.delete(joint.length() - length, joint.length());
+            } else {
+                // 第一次查找就失败，不需要做任何处理。
+                // 这种情况是给定的字符串长度超过了12
+                System.out.println("给定字符串长度超过IP最大有效长度12，给定字符串长度为：" + str.length());
+            }
+        }*/
     }
 
     /**
@@ -48,60 +122,5 @@ public class LeetCode93 {
         List<String> result = Lists.newArrayList();
 
         return result;
-    }
-
-
-    /**
-     * 个人思路：递归
-     *
-     * @param s 给定的IP字符串
-     * @return 结果
-     */
-    @Deprecated
-    private static List<String> individualExecute(String s) {
-        List<String> result = Lists.newArrayList();
-        StringBuilder joint = new StringBuilder();
-        // 记录字符串拆解次数
-        Deque<String> stack = Lists.newLinkedList();
-        findValidIpStr(result, joint, s, stack);
-        return result;
-    }
-
-    @Deprecated
-    private static void findValidIpStr(List<String> result, StringBuilder joint, String str, Deque<String> stack) {
-        // 递归边界：字符串拆解3次即完成一个ip的查询
-        if (stack.size() == 3) {
-            // 符合IP规则，则加入到结果集中：
-            // （1）不为空
-            // （2）不等于0的时候首位不是0，首位是0的时候只能是0
-            // （3）数字小于等于255
-            if (str != null && !"".equals(str) && (str.equals("0") || (str.length() > 1 && str.charAt(0) != '0')) && Integer.parseInt(str) <= 255) {
-                joint.append(str);
-                result.add(joint.toString());
-            }
-            return;
-        }
-        if (str.length() <= (4 - stack.size()) * 3 && str.length() > 0) {
-            // 每层循环中，该层字符个数不能超过3
-            for (int i = 0; i < Math.min(3, str.length()); i++) {
-                String currentStr = str.substring(0, i + 1);
-                // 当层循环中：
-                // （1）数值大于255，则已经不符合IP规则，停止循环
-                // （2）该数值的首位是0(0可以，但是01，012这种就不行)，则已经不符合IP规则，停止循环
-                if (Integer.parseInt(currentStr) > 255 || (currentStr.length() > 1 && currentStr.charAt(0) != '0')) {
-                    break;
-                }
-                joint.append(currentStr).append(".");
-                stack.push(currentStr);
-                String remainStr = str.substring(i + 1);
-                findValidIpStr(result, joint, remainStr, stack);
-                // 底层递归完成，将底层字符串在堆栈中的内容清除
-                stack.pop();
-                // 完成一次调用后，将刚加入的字符串从joint中删除
-                joint.delete(joint.length() - remainStr.length(), joint.length());
-            }
-        } else {
-            joint.delete(joint.lastIndexOf("."), joint.length());
-        }
     }
 }
