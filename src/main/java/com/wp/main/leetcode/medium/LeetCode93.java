ecbd90bb39a86e69ace13675346cc1d7e1f49eb4
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-05 14:20:15 +0800

    LeetCode93：复原IP地址

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode93.java
@@ -0,0 +1,43 @@
+package com.wp.main.leetcode.medium;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @Description 复原 IP 地址
+ * 有效IP地址正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+ * 例如："0.1.2.201" 和 "192.168.1.1" 是有效IP地址，但是"0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是无效IP地址。
+ * 给定一个只包含数字的字符串s用以表示一个IP地址，返回所有可能的有效IP地址，这些地址可以通过在s中插入'.'来形成。
+ * 你不能重新排序或删除s中的任何数字。你可以按任何顺序返回答案。
+ * <p>
+ * 示例 1：
+ * 输入：s = "25525511135"
+ * 输出：["255.255.11.135","255.255.111.35"]
+ * <p>
+ * 示例 2：
+ * 输入：s = "0000"
+ * 输出：["0.0.0.0"]
+ * <p>
+ * 示例 3：
+ * 输入：s = "101023"
+ * 输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 20
+ * s 仅由数字组成
+ * @Author admin
+ * @Date 2023/3/5 14:16
+ */
+public class LeetCode93 {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<String> individualExecute() {
+        List<String> result = Lists.newArrayList();
+
+
+        return result;
+    }
+}
