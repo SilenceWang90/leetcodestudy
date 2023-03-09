a0e0cea21f1e8a8bedb2b7d05aab560782f8a8d9
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-09 14:51:11 +0800

    算法学习：选择排序

---
+++ src/main/java/com/wp/main/algorithm/mukewang/util/SortedUtil.java
@@ -0,0 +1,27 @@
+package com.wp.main.algorithm.mukewang.util;
+
+/**
+ * @Description 排序工具类
+ * @Author admin
+ * @Date 2023/3/9 14:48
+ */
+public class SortedUtil {
+    private SortedUtil() {
+    }
+
+
+    /**
+     * 判断数组是否排序(升序)成功
+     *
+     * @param nums 目标数组
+     * @return true成功/false失败
+     */
+    public static boolean arraySorted(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
