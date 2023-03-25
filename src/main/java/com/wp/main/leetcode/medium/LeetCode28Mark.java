e8469b1f797e2be1864dd06d2667c3297de0fd85
Wangpeng@90 <wangpeng0717@sina.cn>
2023-03-25 17:34:30 +0800

    LeetCode28：KMP解法

--- src/main/java/com/wp/main/leetcode/medium/LeetCode28Mark.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode28Mark.java
@@ -1,19 +1,28 @@
 package com.wp.main.leetcode.medium;
 
 /**
+ * Mark：KMP算法
+ *
+ */
+
+/**
  * @Description
  * @Author admin
  * @Date 2023/3/25 16:53
  */
-public class LeetCode28 {
+public class LeetCode28Mark {
     public static void main(String[] args) {
 //        System.out.println(individualExecution("leetcode", "leeto"));
 //        System.out.println(individualExecution("sadbutsad", "sad"));
-        System.out.println(individualExecution("mississippi", "issi"));
+        System.out.println(individualExecute("mississippi", "issi"));
+    }
+
+    private static int standardExecute(String haystack, String needle){
+
     }
 
 
-    private static int individualExecution(String haystack, String needle) {
+    private static int individualExecute(String haystack, String needle) {
         int i = 0, j = 0, n = 0;
         while (i <= haystack.length() - needle.length()) {
             if (haystack.charAt(i) != needle.charAt(n)) {
