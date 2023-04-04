1c32008ca7c1862f91d0eef1a039885ebe41ff45
Wangpeng@90 <wangpeng0717@sina.cn>
2023-04-04 16:28:12 +0800

    LeetCode31：下一个排列。大神解法很简单，也很容易理解

--- src/main/java/com/wp/main/leetcode/medium/LeetCode31.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode31.java
@@ -1,5 +1,7 @@
 package com.wp.main.leetcode.medium;
 
+import java.util.Arrays;
+
 /**
  * @Description 下一个排列
  * 整数数组的一个 排列就是将其所有成员以序列或线性顺序排列。
@@ -31,20 +33,54 @@ public class LeetCode31 {
         int[] nums = {1, 2, 3};
 //        int[] nums = {3,2,1};
 //        int[] nums = {1, 1, 5};
-        individualExecute(nums);
+//        individualExecute(nums);
         for (int num : nums) {
             System.out.print(num + " ");
         }
     }
 
     /**
-     * 个人思路：
+     * 算法推导
+     * 如何得到这样的排列顺序？这是本文的重点。我们可以这样来分析：
+     * <p>
+     * 我们希望下一个数 比当前数大，这样才满足 “下一个排列” 的定义。因此只需要 将后面的「大数」与前面的「小数」交换，就能得到一个更大的数。比如123456，将5和6交换就能得到一个更大的数123465。
+     * 我们还希望下一个数 增加的幅度尽可能的小，这样才满足“下一个排列与当前排列紧邻“的要求。为了满足这个要求，我们需要：
+     * 在 尽可能靠右的低位 进行交换，需要从后向前查找
+     * 将一个尽可能小的「大数」与前面的「小数」交换。比如 123465，下一个排列应该把5和4交换而不是把6和4交换
+     * 将「大数」换到前面后，需要将「大数」后面的所有数 重置为升序，升序排列就是最小的排列。以 123465 为例：首先按照上一步，交换 5和4，得到123564；然后需要将5之后的数重置为升序，得到123546。
+     * 显然123546比123564更小，123546就是123465的下一个排列
+     * 以上就是求 “下一个排列” 的分析过程。
+     *
+     * @param nums
+     */
+    private static void standardExecute(int[] nums) {
+        int len = nums.length;
+        for (int i = len - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                Arrays.sort(nums, i, len);
+                for (int j = i; j < len; j++) {
+                    if (nums[j] > nums[i - 1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[i - 1];
+                        nums[i - 1] = temp;
+                        return;
+                    }
+                }
+            }
+        }
+        Arrays.sort(nums);
+        return;
+    }
+
+    /**
+     * 个人思路：未通过
      * 1、从右侧遍历数组，判断相邻2个数字的大小：
      * 1.1、如果后面的数字b比前面的数字a大：记录这两个数字
      * （1）继续遍历数组，如果存在后面的数字小于等于前面的数字或数组已遍历完成，则终止循环，交换第一次记录的两个数字的位置
      * （2）继续遍历数组，如果均是后面的数字比前面的数字大，则交换最后两个数字的位置
      * 1.2、如果后面的数字都比前一个数字小，则交换第一和第二个数字的位置，用插入排序实现
      */
+    @Deprecated
     private static void individualExecute(int[] nums) {
         Integer a = null;
         Integer b = null;
