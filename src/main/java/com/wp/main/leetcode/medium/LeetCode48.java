0a4a5f2a93e1788bf23ef7f1ca4251ca9f6135c1
Wangpeng@90 <wangpeng0717@sina.cn>
2023-04-27 15:09:45 +0800

    LeetCode48：旋转图像

--- src/main/java/com/wp/main/leetcode/medium/LeetCode48.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode48.java
@@ -23,10 +23,40 @@ package com.wp.main.leetcode.medium;
 public class LeetCode48 {
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        individualExecute(matrix);
     }
 
+    /**
+     * 个人思路：x为行，y为列
+     * 1、旋转90度即行列转置，同一行转成同一列，同一列转成同一行
+     * 2、按行处理：
+     * 2.1、同一行转到同一列，列的值为：y=3-x
+     * 2.2、同一行按列遍历的时候x自增：x=++x
+     * 3、元素置换：增加辅助数组进行转置
+     *
+     * @param matrix 目标矩阵
+     */
     private static void individualExecute(int[][] matrix) {
-
-
+        // 矩阵维度转成数组的长度，便于数组元素进行转置的时候行与列的变换
+        int n = matrix.length;
+        // 新增辅助矩阵，协助存储移位后的数据
+        int[][] matrixNew = new int[n][n];
+        // 遍历二维数组，获取每一个维度的数组信息
+        for (int x = 0; x < matrix.length; x++) {
+            // 获取一维数组
+            int[] xArray = matrix[x];
+            // 遍历一维数组，获取数组中的每一个元素
+            for (int y = 0; y < xArray.length; y++) {
+                int current = matrix[x][y];
+                matrixNew[y][n - x - 1] = current;
+            }
+        }
+        for (int x = 0; x < matrixNew.length; x++) {
+            int temp[] = matrixNew[x];
+            for (int y = 0; y < temp.length; y++) {
+                System.out.print(temp[y] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
