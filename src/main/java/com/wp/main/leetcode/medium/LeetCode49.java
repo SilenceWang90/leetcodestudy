5599e38d646df39b5e81b2ee5aaab157e5bee49e
Wangpeng@90 <wangpeng0717@sina.cn>
2023-04-28 11:25:48 +0800

    LeetCode4：母异位词分组

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode49.java
@@ -0,0 +1,42 @@
+package com.wp.main.leetcode.medium;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @Description 字母异位词分组
+ * 给你一个字符串数组，请你将字母异位词组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+ * <p>
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * <p>
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * <p>
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ * <p>
+ * 提示：
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i]仅包含小写字母
+ * @Author admin
+ * @Date 2023/4/28 11:24
+ */
+public class LeetCode49 {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<List<String>> individualExecute(String[] strs) {
+        List<List<String>> result = Lists.newArrayList();
+
+
+        return result;
+    }
+}
