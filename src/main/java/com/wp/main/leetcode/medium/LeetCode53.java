fe4ec978e692d4c617506b62e1efc44a0b46f29d
Wangpeng@90 <wangpeng0717@sina.cn>
2023-05-13 12:37:25 +0800

    LeetCode53：最大子数组和

--- src/main/java/com/wp/main/leetcode/medium/LeetCode53.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode53.java
@@ -26,9 +26,9 @@ package com.wp.main.leetcode.medium;
  */
 public class LeetCode53 {
     public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 //        int[] nums = {1};
-//        int[] nums = {5,4,-1,7,8};
+        int[] nums = {5,4,-1,7,8};
         System.out.println(individualExecute(nums));
     }
 
