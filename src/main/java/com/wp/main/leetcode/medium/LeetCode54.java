4121c1de5ae4bfb4169378d07ea407588b757868
Wangpeng@90 <wangpeng0717@sina.cn>
2023-06-03 12:28:05 +0800

    LeetCode54：螺旋矩阵

--- src/main/java/com/wp/main/leetcode/medium/LeetCode54.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode54.java
@@ -19,7 +19,9 @@ import java.util.List;
  */
 public class LeetCode54 {
     public static void main(String[] args) {
-
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(individualExecute(matrix));
     }
 
     /**
