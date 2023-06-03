64929073f4c301113f36dbfa95f8bedfe39dd60f
Wangpeng@90 <wangpeng0717@sina.cn>
2023-06-03 12:18:42 +0800

    LeetCode54：螺旋矩阵

--- src/main/java/com/wp/main/leetcode/medium/LeetCode54.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode54.java
@@ -28,6 +28,7 @@ public class LeetCode54 {
      * （2）x向左移动到边界则上移，即y指针--
      * （3）y向下移动到边界则静止不动
      * （4）y向上移动到边界则静止不动
+     * 取走的值赋值-111，因为题中定义-100 <= matrix[i][j] <= 100，只要前方要取的值是-111即视为被取走了，那么就可以进行转向操作
      *
      * @param matrix
      * @return
@@ -40,13 +41,27 @@ public class LeetCode54 {
         int x = 0, y = 0;
         // 只要集合中数量没达标，则循环逻辑继续
         while (result.size() <= xLength * yLength) {
-            if (x < xLength && y == 0) {
+            // 取值，并将该位置的值标记为已取走
+            if (matrix[x][y] != -111) {
                 result.add(matrix[x][y]);
+                matrix[x][y] = -111;
+            }
+            // 顺时针指定移动逻辑
+            // 右移
+            if (matrix[x + 1][y] != -111 && x + 1 < xLength) {
                 x++;
             }
-            if (x == xLength - 1 && y < yLength) {
+            // 下移
+            else if (matrix[x][y + 1] != -111 && y + 1 < yLength) {
                 y++;
-                result.add(matrix[x][y]);
+            }
+            // 左移
+            else if (matrix[x - 1][y] != -111 && x > 0) {
+                x--;
+            }
+            // 上移
+            else if (matrix[x][y - 1] != -111 && y > 0) {
+                y--;
             }
         }
 
