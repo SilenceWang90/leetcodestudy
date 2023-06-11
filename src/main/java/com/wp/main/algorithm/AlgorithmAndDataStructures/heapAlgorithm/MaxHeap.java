3bf8f4ff25b7035968393fa85905ec84479a6564
Wangpeng@90 <wangpeng0717@sina.cn>
2023-06-11 11:34:35 +0800

    堆与堆排序：shiftUp操作

--- src/main/java/com/wp/main/algorithm/AlgorithmAndDataStructures/heapAlgorithm/MaxHeap.java
+++ src/main/java/com/wp/main/algorithm/AlgorithmAndDataStructures/heapAlgorithm/MaxHeap.java
@@ -25,6 +25,7 @@ public class MaxHeap {
     /**
      * 查看堆中目前多少个元素(堆对象为private，所以外部无法通过data.size()查看元素个数)
      * 注意这个count和数组的size不同，数组的size是容量一共可以放入多少个元素，这个count是当前数据中真实存在的元素数量
+     *
      * @return
      */
     public int size() {
@@ -33,9 +34,42 @@ public class MaxHeap {
 
     /**
      * 判断堆是否为空
+     *
      * @return
      */
     public boolean isEmpty() {
         return count == 0;
     }
+
+    /**
+     * 插入元素
+     *
+     * @param item 待插入的元素
+     */
+    public void insert(int item) {
+        // 1、元素放入到数组中(最后一个元素的后面)
+        data[count + 1] = item;
+        // 容量+1
+        count++;
+        // 2、移动新增的元素至合适的位置
+        shiftUp(count);
+    }
+
+    /**
+     * 向上移动新增的元素
+     *
+     * @param position 新增元素所在的位置
+     */
+    private void shiftUp(int position) {
+        // 如果当前位置的元素比父节点小，那么就交换当前节点和父节点的位置。
+        // 按照此逻辑不断的移动新增的元素直到合适的位置即可
+        while (data[position / 2] < data[position]) {
+            int temp = data[position / 2];
+            data[position / 2] = data[position];
+            data[position] = temp;
+            // 移动过程中修改新增元素当前所在的最新位置
+            position = position / 2;
+        }
+    }
+
 }
