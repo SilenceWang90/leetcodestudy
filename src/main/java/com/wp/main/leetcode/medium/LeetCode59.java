7a1889e6b9c5455dc83cdba1d25d24def32d518c
Wangpeng@90 <wangpeng0717@sina.cn>
2023-06-19 13:22:12 +0800

    LeetCode59：螺旋矩阵 II

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode59.java
@@ -0,0 +1,28 @@
+package com.wp.main.leetcode.medium;
+
+/**
+ * @Description 螺旋矩阵 II
+ * 给你一个正整数n，生成一个包含1到n2所有元素，且元素按顺时针顺序螺旋排列的n x n正方形矩阵matrix。
+ * <p>
+ * 示例 1：
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * <p>
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：[[1]]
+ * <p>
+ * 提示：
+ * 1 <= n <= 20
+ * @Author admin
+ * @Date 2023/6/19 13:20
+ */
+public class LeetCode59 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int[][] individualExecution(int n) {
+        return null;
+    }
+}
