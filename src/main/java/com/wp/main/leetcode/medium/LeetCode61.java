da8f553a41b5e8bf3cf8c10f0b8008167589fc36
Wangpeng@90 <wangpeng0717@sina.cn>
2023-06-24 11:54:00 +0800

    LeetCode61：旋转链表

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode61.java
@@ -0,0 +1,33 @@
+package com.wp.main.leetcode.medium;
+
+import com.wp.main.common.ListNode;
+
+/**
+ * @Description 旋转链表
+ * 给你一个链表的头节点head，旋转链表，将链表每个节点向右移动k个位置
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[4,5,1,2,3]
+ * <p>
+ * 示例 2：
+ * 输入：head = [0,1,2], k = 4
+ * 输出：[2,0,1]
+ * <p>
+ * 提示：
+ * 链表中节点的数目在范围[0, 500]内
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 109
+ * @Author admin
+ * @Date 2023/6/24 11:50
+ */
+public class LeetCode61 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-1);
+        System.out.println(head);
+    }
+
+    private static ListNode individualExecution(ListNode head, int k) {
+
+    }
+}
