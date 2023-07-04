97b7e81b43530a8727d01e4c5ce284463c605674
wangpeng <wangpeng0717@sina.cn>
2023-07-04 15:58:27 +0800

    LeetCode69：x的平方根

---
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode69.java
@@ -0,0 +1,33 @@
+package com.wp.main.leetcode.easy;
+
+/**
+ * @Description x的平方根
+ * 给你一个非负整数 x ，计算并返回x的算术平方根 。
+ * 由于返回类型是整数，结果只保留整数部分，小数部分将被舍去。
+ * <p>
+ * 注意：不允许使用任何内置指数函数和算符，例如pow(x, 0.5) 或者 x ** 0.5 。
+ * <p>
+ * 示例 1：
+ * 输入：x = 4
+ * 输出：2
+ * <p>
+ * 示例 2：
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ * <p>
+ * 提示：
+ * 0 <= x <= 231 - 1
+ * @Author wangpeng
+ * @Date 2023/7/4 15:13
+ */
+public class LeetCode69 {
+    public static void main(String[] args) {
+        System.out.println(individualExecution(4));
+        System.out.println(individualExecution(8));
+    }
+
+    private static int individualExecution(int x) {
+
+    }
+}
