dd5ee7010eeea4c41aca12b95cced7f465a7e607
Wangpeng@90 <wangpeng0717@sina.cn>
2023-07-18 09:24:35 +0800

    LeetCode71：简化路径

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode71.java
@@ -0,0 +1,21 @@
+package com.wp.main.leetcode.medium;
+
+/**
+ * @Description 简化路径
+ * @Author admin
+ * @Date 2023/7/18 9:23
+ */
+public class LeetCode71 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 个人思路：
+     * @param path 路径参数
+     * @return 标准路径
+     */
+    private static String individualExecution(String path){
+
+    }
+}
