aede4f4cf8d8a67bdaf5a6dc81f2744d46de7a79
Wangpeng@90 <wangpeng0717@sina.cn>
2023-08-23 09:34:25 +0800

    LeetCode78：子集

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode78.java
@@ -0,0 +1,28 @@
+package com.wp.main.leetcode.medium;
+
+/**
+ * @Description 子集
+ * 给你一个整数数组nums，数组中的元素互不相同。返回该数组所有可能的子集（幂集）。
+ * 解集不能包含重复的子集。你可以按任意顺序返回解集。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ *
+ * 提示：
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * nums中的所有元素互不相同
+ *
+ * @Author admin
+ * @Date 2023/8/23 9:33
+ */
+public class LeetCode78 {
+    public static void main(String[] args) {
+
+    }
+}
