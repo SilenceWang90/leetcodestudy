c2eb040296b56cabb10320b0ccbef89d715a8d04
Wangpeng@90 <wangpeng0717@sina.cn>
2023-08-23 10:14:19 +0800

    LeetCode78：子集

--- src/main/java/com/wp/main/leetcode/medium/LeetCode78.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode78.java
@@ -38,11 +38,11 @@ public class LeetCode78 {
      */
     private static List<List<Integer>> individualExecution(int[] nums) {
         List<List<Integer>> result = Lists.newArrayList();
-        // 空集合也算
-        result.add(new ArrayList<>());
         // 当前已选的组合
         List<Integer> currentCombination = Lists.newArrayList();
-        recursion(nums, result, currentCombination);
+        recursion(nums, result, currentCombination, 1, 0);
+        // 空集合也算
+        result.add(new ArrayList<>());
         return result;
     }
 
@@ -52,8 +52,34 @@ public class LeetCode78 {
      * @param nums               给定数组
      * @param result             结果集
      * @param currentCombination 当前遍历生成的集合
+     * @param n                  当前要选几个元素
+     * @param start              下一层递归起始位置
      */
-    private static void recursion(int[] nums, List<List<Integer>> result, List<Integer> currentCombination) {
+    private static void recursion(int[] nums, List<List<Integer>> result, List<Integer> currentCombination, int n, int start) {
+        /**1、递归终止条件**/
+        if (currentCombination.size() == n) {
+            List<Integer> temp = Lists.newArrayList(currentCombination);
+            result.add(temp);
+            return;
+        }
 
+        /**2、递归处理逻辑**/
+        // 2.1、n为统计每次要选组合的数字数量，每次递归结束则组合中数字数量也会增加
+        while (n <= nums.length) {
+            for (int i = start; i < nums.length; i++) {
+                // 向当前集合加入选择的元素
+                currentCombination.add(i);
+                // 下一层递归起始位置+1，因为要求不能重复
+                start++;
+                // 递归选择
+                recursion(nums, result, currentCombination, n, start);
+                /**3、递归结束操作*/
+                // 将最后添加的元素清除
+                currentCombination.remove(currentCombination.size() - 1);
+            }
+            // 针对新一轮的选择，组合数量+1，start起始位置归0
+            n++;
+            start = 0;
+        }
     }
 }
