60a9fa4ffb7849ebbb8c9eb09fa6c54d10d7ac0c
Wangpeng@90 <wangpeng0717@sina.cn>
2023-08-25 14:41:24 +0800

    LeetCode79：单词搜索

--- src/main/java/com/wp/main/leetcode/medium/LeetCode79.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode79.java
@@ -2,14 +2,16 @@ package com.wp.main.leetcode.medium;
 
 /**
  * @Description 单词搜索
- * 给定一个m x n二维字符网格board和一个字符串单词word。如果word存在于网格中，返回true；否则，返回false 。
+ * 给定一个m x n二维字符网格board和一个字符串单词word。如果word存在于网格中，返回true；否则，返回false。
  * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
  * @Author admin
  * @Date 2023/8/25 10:30
  */
 public class LeetCode79 {
     public static void main(String[] args) {
-
+        char[][] board= {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "ABCCED";
+        System.out.println(individualExecution(board, word));
     }
 
     /**
