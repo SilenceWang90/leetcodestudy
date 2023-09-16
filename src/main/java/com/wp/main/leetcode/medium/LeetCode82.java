f955ba17e1f31d51034e964fd6215ce52b9f5644
Wangpeng@90 <wangpeng0717@sina.cn>
2023-09-16 13:38:06 +0800

    LeetCode82：删除排序链表中的重复元素 II

--- src/main/java/com/wp/main/leetcode/medium/LeetCode82.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode82.java
@@ -2,6 +2,9 @@ package com.wp.main.leetcode.medium;
 
 import com.wp.main.common.ListNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description 删除排序链表中的重复元素 II
  * 给定一个已排序的链表的头head，删除原始链表中所有重复数字的节点，只留下不同的数字。返回已排序的链表。
@@ -24,7 +27,7 @@ import com.wp.main.common.ListNode;
  */
 public class LeetCode82 {
     public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(1);
+        /*ListNode listNode1 = new ListNode(1);
         ListNode listNode2 = new ListNode(2);
         ListNode listNode3 = new ListNode(3);
         ListNode listNode4 = new ListNode(3);
@@ -43,11 +46,55 @@ public class LeetCode82 {
         while (result != null) {
             System.out.println(result.getValue() + " ");
             result = result.getNext();
-        }
+        }*/
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        a = b;
+        System.out.println(a.getValue());
+        System.out.println(a == b);
+        b = c;
+        System.out.println(a.getValue());
+        System.out.println(a == c);
+        System.out.println(b.getValue());
+        System.out.println(a == b);
     }
 
     private static ListNode individualExecution(ListNode head) {
-        return head;
+        // 设置一个top节点，便于对第一个节点的操作和其他节点相同
+        ListNode top = new ListNode(-1);
+        // 链表的尾结点，用于插入有效节点
+        ListNode tail = top;
+        // 记录出现的节点值出现的次数
+        Map<Integer, Integer> recordMap = new HashMap<>();
+
+        // 当前遍历的节点
+        ListNode current = head;
+        // 当前遍历节点相邻的前一个节点
+        ListNode prev = top;
+
+        while (current != null) {
+            if (recordMap.containsKey(current.getValue())) {
+                /** 1、当前节点的元素出现过 **/
+
+            } else {
+                /** 2、当前节点的元素未出现过 **/
+                // 2.1、prev节点是否重复，如果没有重复则添加到队列中
+                if (recordMap.get(prev.getValue()) <= 1) {
+                    tail.setNext(prev);
+                    tail = tail.getNext();
+                }
+
+            }
+            // 节点后移
+            prev = current;
+            current = current.getNext();
+        }
+
+
+        return top.getNext();
+
     }
 }
 
