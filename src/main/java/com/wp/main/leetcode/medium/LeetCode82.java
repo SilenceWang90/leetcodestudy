3db7777c65427d4b3a48fee73a4d6033a0a7fdff
Wangpeng@90 <wangpeng0717@sina.cn>
2023-09-16 09:50:15 +0800

    LeetCode82：删除排序链表中的重复元素 II

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode82.java
@@ -0,0 +1,35 @@
+package com.wp.main.leetcode.medium;
+
+import com.wp.main.common.ListNode;
+
+/**
+ * @Description 删除排序链表中的重复元素 II
+ * 给定一个已排序的链表的头head，删除原始链表中所有重复数字的节点，只留下不同的数字。返回已排序的链表。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,1,1,2,3]
+ * 输出：[2,3]
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ * @Author admin
+ * @Date 2023/9/16 9:48
+ */
+public class LeetCode82 {
+    public static void main(String[] args) {
+
+    }
+
+    private static ListNode individualExecution(ListNode head) {
+        return head;
+    }
+}
+
+
