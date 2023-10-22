aa20c8ecd8962d19cd037d34f0f3795b723f37a4
Wangpeng@90 <wangpeng0717@sina.cn>
2023-10-22 16:41:24 +0800

    LeetCode92：反转链表 II

---
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode92.java
@@ -0,0 +1,33 @@
+package com.wp.main.leetcode.medium;
+
+import com.wp.main.common.ListNode;
+
+/**
+ * @Description 反转列表 II
+ * 给你单链表的头指针head和两个整数left和right，其中left <= right。请你反转从位置left到位置right的链表节点，返回反转后的链表。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ * <p>
+ * 示例 2：
+ * 输入：head = [5], left = 1, right = 1
+ * 输出：[5]
+ * <p>
+ * 提示：
+ * 链表中节点数目为 n
+ * 1 <= n <= 500
+ * -500 <= Node.val <= 500
+ * 1 <= left <= right <= n
+ * @Author admin
+ * @Date 2023/10/22 16:39
+ */
+public class LeetCode92 {
+    public static void main(String[] args) {
+
+    }
+
+    private static ListNode individualExecution(ListNode head, int left, int right) {
+
+    }
+}
