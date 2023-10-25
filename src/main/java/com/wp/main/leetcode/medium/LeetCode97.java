9aeeac75a3812cc5d8438c9bcceb6b6b1d0a6e81
wangpeng <wangpeng0717@sina.cn>
2023-10-25 16:51:36 +0800

    LeetCode97：交错字符串

--- src/main/java/com/wp/main/leetcode/medium/LeetCode97.java
+++ src/main/java/com/wp/main/leetcode/medium/LeetCode97.java
@@ -9,26 +9,25 @@ package com.wp.main.leetcode.medium;
  * |n - m| <= 1
  * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
  * 注意：a + b 意味着字符串 a 和 b 连接。
- *
+ * <p>
  * 示例 1：
  * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
  * 输出：true
- *
+ * <p>
  * 示例 2：
  * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
  * 输出：false
- *
+ * <p>
  * 示例 3：
  * 输入：s1 = "", s2 = "", s3 = ""
  * 输出：true
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 0 <= s1.length, s2.length <= 100
  * 0 <= s3.length <= 200
  * s1、s2、和 s3 都由小写英文字母组成
- *
  * @Author wangpeng
  * @Date 2023/10/25 16:46
  */
@@ -37,8 +36,12 @@ public class LeetCode97 {
 
     }
 
-    private static boolean individualExecution(String s1, String s2, String s3){
+    private static boolean individualExecution(String s1, String s2, String s3) {
+        boolean result = false;
+        for (int i = 0; i < s3.length(); i++) {
+            char current = s3.charAt(i);
+        }
 
-
+        return result;
     }
 }
