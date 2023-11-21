1e13c3a7dbee1c489f91ea010644cc75ec449f0b
wangpeng <wangpeng0717@sina.cn>
2023-11-21 17:59:21 +0800

    LeetCode94：二叉树

---
+++ src/main/java/com/wp/main/common/TreeNode.java
@@ -0,0 +1,28 @@
+package com.wp.main.common;
+
+import lombok.Data;
+
+/**
+ * @Description 二叉树定义
+ * @Author wangpeng
+ * @Date 2023/11/21 16:42
+ */
+@Data
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
