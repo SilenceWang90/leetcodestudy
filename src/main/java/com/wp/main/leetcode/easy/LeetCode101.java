dff468eb933931115d9a20dcab27eb72e2f3ad04
wangpengyml <wangpeng0717@sina.cn>
2024-03-25 13:36:52 +0800

    leetcode101：对称二叉树

--- src/main/java/com/wp/main/leetcode/easy/LeetCode101.java
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode101.java
@@ -5,16 +5,19 @@ import com.wp.main.common.TreeNode;
 /**
  * @author wangpeng
  * @description 对称二叉树
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
  * @date 2024/3/25 1:23 PM
  **/
 public class LeetCode101 {
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
+
     }
 
     /**
      * 个人思路：递归
+     *
      * @param treeNode 树根结点
      */
     public static void individualExecution(TreeNode treeNode) {
