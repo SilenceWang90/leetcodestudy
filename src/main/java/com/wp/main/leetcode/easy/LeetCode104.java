279cac77bcbb8d5739b84aeefa5f84ec97c529eb
wangpeng <wangpeng0717@sina.cn>
2024-03-25 20:38:19 +0800

    leetcode104：二叉树的最大深度

---
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode104.java
@@ -0,0 +1,28 @@
+package com.wp.main.leetcode.easy;
+
+import com.wp.main.common.TreeNode;
+
+/**
+ * @author wangpeng
+ * @description LeetCode104 二叉树的最大深度
+ * 给定一个二叉树 root ，返回其最大深度。
+ * <p>
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ * @date 2024/3/25 4:24 PM
+ **/
+public class LeetCode104 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 个人思路：递归
+     * @param treeNode 根结点
+     * @return 层数
+     */
+    public static int individualExecution(TreeNode treeNode){
+        int depth = 0;
+
+        return depth;
+    }
+}
