3b1875080ec416b78147139a58631c9f580ddde9
wangpeng <wangpeng0717@sina.cn>
2024-03-25 13:35:23 +0800

    leetcode101：对称二叉树

---
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode101.java
@@ -0,0 +1,25 @@
+package com.wp.main.leetcode.easy;
+
+import com.wp.main.common.TreeNode;
+
+/**
+ * @author wangpeng
+ * @description 对称二叉树
+ * @date 2024/3/25 1:23 PM
+ **/
+public class LeetCode101 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+    }
+
+    /**
+     * 个人思路：递归
+     * @param treeNode 树根结点
+     */
+    public static void individualExecution(TreeNode treeNode) {
+
+
+    }
+
+}
