b5095ffd137c385dac88b56917345303f8995a87
wangpeng <wangpeng0717@sina.cn>
2025-12-11 14:32:39 +0800

    提交"3333"

--- src/main/java/com/wp/main/leetcode/easy/LeetCode1Mark.java
+++ src/main/java/com/wp/main/leetcode/easy/LeetCode1Mark.java
@@ -24,6 +24,7 @@ public class LeetCode1Mark {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("2222");
+        System.out.println("3333");
         Integer[] nums = {1, 3, 4, 5, 6, 8, 9};
         Integer target = 10;
         Integer[] result = standardExecute(nums, target);
